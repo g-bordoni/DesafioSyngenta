06cac7ccfc39800737deec3386a8fd7aacd6991e
Gabriel Bordoni <bordoni.gabriel@hotmail.com>
2021-06-14 20:37:25 -0300

    Reajustes no main

--- ProblemaDaReserva/hoteisCadastrados.py
+++ ProblemaDaReserva/hoteisCadastrados.py
@@ -5,7 +5,7 @@ class Lakewood(Hotel):
     def __init__(self):
         ESTRELAS = 3
         VALORES_REGULARES = (110,90)
-        VALORES_FIDELIDADE = (80, 80)
+        VALORES_FIDELIDADE = (200,200)
 
         super().__init__(ESTRELAS, VALORES_REGULARES, VALORES_FIDELIDADE)
     
@@ -17,7 +17,7 @@ class Bridgewood(Hotel):
     def __init__(self):
         ESTRELAS = 4
         VALORES_REGULARES = (160,60)
-        VALORES_FIDELIDADE = (110, 50)
+        VALORES_FIDELIDADE = (200,200)
 
         super().__init__(ESTRELAS, VALORES_REGULARES, VALORES_FIDELIDADE)
     
@@ -30,13 +30,23 @@ class Ridgewood(Hotel):
     def __init__(self):
         ESTRELAS = 5
         VALORES_REGULARES = (220,150)
-        VALORES_FIDELIDADE = (100, 40)
+        VALORES_FIDELIDADE = (200,200)
 
         super().__init__(ESTRELAS, VALORES_REGULARES, VALORES_FIDELIDADE)
 
     def __str__(self) -> str:
         return "Ridgewood"
 
+class OuroMinas(Hotel):
+
+    def __init__(self):
+        ESTRELAS = 6
+        VALORES_REGULARES = (250,140)
+        VALORES_FIDELIDADE = (150,100)
+
+        super().__init__(ESTRELAS, VALORES_REGULARES, VALORES_FIDELIDADE)
+    def __str__(self)->str:
+        return "OuroMinas"
     
 
 
--- ProblemaDaReserva/main.py
+++ ProblemaDaReserva/main.py
@@ -1,17 +1,25 @@
 from hoteisCadastrados import *
 
+def melhor_orcamento(entrada:str, opcoes: list):
+    if(len(opcoes)==0):
+        return None
+
+    opcoes[0].fazer_orcamento(entrada)
+    melhor_opcao = opcoes[0]
+    for i in range(1, len(opcoes)):
+        opcoes[i].fazer_orcamento(entrada)
+        if opcoes[i]>melhor_opcao:
+            melhor_opcao = opcoes[i]
+    return melhor_opcao
 
 opcoes_hoteis = []
 opcoes_hoteis += [Lakewood()]
 opcoes_hoteis += [Bridgewood()]
 opcoes_hoteis += [Ridgewood()]
+opcoes_hoteis += [OuroMinas()]
 
 entrada = input("\nEntrada:\n")
-for i in range(len(opcoes_hoteis)):
-    opcoes_hoteis[i].fazer_orcamento(entrada)
+hotel_escolhido = melhor_orcamento(entrada,opcoes_hoteis)
 
-melhor_opcao = opcoes_hoteis[0] if (opcoes_hoteis[0]>opcoes_hoteis[1] and opcoes_hoteis[0]>opcoes_hoteis[2])\
-    else opcoes_hoteis[1] if (opcoes_hoteis[1]>opcoes_hoteis[2])\
-        else opcoes_hoteis[2]
 
-print(str(melhor_opcao))
+print(str(hotel_escolhido))
